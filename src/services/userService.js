cca287d24d8458cb80827977edaf1a51eec01ef3
grantmn11 <grantferreira11@gmail.com>
2021-01-23 13:46:00 -0800

    ready for dep

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,7 @@
 import {setToken, getUserFromToken, removeToken, getToken} from './tokenService'
 
-const BASE_URL = 'http://localhost:3001/api/users'
-
+// const BASE_URL = 'http://localhost:3001/api/users'
+const BASE_URL = 'https://project-3-onestop.herokuapp.com/api/users'
 
 function signup(user){
     return fetch(BASE_URL + '/signup', {
